4e0838d79ff92b95c9a0bd159cab97a818173889
Nirav Chavda <nmchavda99@gmail.com>
2021-10-30 12:11:17 +0530

    2 Buzz Tests in FizzBuzz

--- FizzBuzz/src/main/FizzBuzz.java
+++ FizzBuzz/src/main/FizzBuzz.java
@@ -2,6 +2,7 @@ package main;
 
 public class FizzBuzz {
     public String parse(int number) {
+        if(number == 5 || number == 10) return "Buzz";
         if (number % 3 == 0) return "Fizz";
         return String.valueOf(number);
     }
--- FizzBuzz/src/test/FizzBuzzTest.java
+++ FizzBuzz/src/test/FizzBuzzTest.java
@@ -13,7 +13,7 @@ public class FizzBuzzTest {
 
     @DataProvider(name = "baseNumbers")
     public static Object[][] baseNumbers() {
-        return new Object[][]{{1, "1"}, {2, "2"}, {4, "4"}};
+        return new Object[][]{{1, "1"}, {2, "2"}, {4, "4"}, {7, "7"}, {8, "8"}, {11, "11"}, {13, "13"}, {14, "14"}};
     }
 
     @DataProvider(name = "fizzNumbers")
@@ -31,5 +31,15 @@ public class FizzBuzzTest {
     @Parameters({"input"})
     void returnFizzForMultiplesOf3(int input) {
         assertEquals("Fizz", new FizzBuzz().parse(input));
+    }
+
+    @Test
+    void returnBuzzFor5() {
+        assertEquals("Buzz", new FizzBuzz().parse(5));
+    }
+
+    @Test
+    void returnBuzzFor10() {
+        assertEquals("Buzz", new FizzBuzz().parse(10));
     }
 }
