4765bb83e5d8de729b3832de470a8806cabc0a47
Nirav Chavda <nmchavda99@gmail.com>
2021-10-30 11:26:26 +0530

    Third Test Refactoring in FizzBuzz

--- FizzBuzz/src/main/FizzBuzz.java
+++ FizzBuzz/src/main/FizzBuzz.java
@@ -2,8 +2,6 @@ package main;
 
 public class FizzBuzz {
     public String parse(int number) {
-        if(number == 2) return "2";
-        if(number == 4) return "4";
-        return "1";
+        return String.valueOf(number);
     }
 }
--- FizzBuzz/src/test/FizzBuzzTest.java
+++ FizzBuzz/src/test/FizzBuzzTest.java
@@ -1,21 +1,32 @@
 package test;
 
 import main.FizzBuzz;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Test
+
 public class FizzBuzzTest {
-    public void return1For1() {
-        assertEquals("1", new FizzBuzz().parse(1));
-    }
+//    @Test
+//    public void return1For1() {
+//        assertEquals("1", new FizzBuzz().parse(1));
+//    }
+//
+//    @Test
+//    public void return2For2() {
+//        assertEquals("2", new FizzBuzz().parse(2));
+//    }
+//
+//    @Test
+//    public void return4For4() {
+//        assertEquals("4", new FizzBuzz().parse(4));
+//    }
 
-    public void return2For2() {
-        assertEquals("2", new FizzBuzz().parse(2));
-    }
-
-    public void return4For4() {
-        assertEquals("4", new FizzBuzz().parse(4));
+    @ParameterizedTest
+    @CsvSource({"1,1", "2,2", "4,4"})
+    void returnStringValueForNumber(int input, String expectedOutput) {
+        assertEquals(expectedOutput, new FizzBuzz().parse(input));
     }
 }
